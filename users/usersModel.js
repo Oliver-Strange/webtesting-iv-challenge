1c380b2cdb77672c744d70ab12a9e07de6bd028c
Oliver-Strange <preston.burton@gmail.com>
2019-06-27 13:25:06 -0700

    modifies server.spec and server. creates knexfile and dbconfig.

---
+++ users/usersModel.js
@@ -0,0 +1,14 @@
+const db = require("../data/dbConfig.js");
+
+module.exports = {
+  insert,
+  remove
+};
+
+async function insert(hobbit) {
+  return null;
+}
+
+function remove(id) {
+  return null;
+}
